b97291d1c5f57067a7ed301b35766c9319449ca9
Yitzy Bratt <yitzybratt32@gmail.com>
2021-06-02 17:11:35 -0400

    axios. console.log relevant info for all 5 frameworks

--- index.js
+++ index.js
@@ -1,3 +1,61 @@
+/* global Chart, axios */
+
+axios.get('https://api.github.com/repos/vuejs/vue').then(function (response) {
+  console.log(response.data);
+  let vueWatchers = response.data.watchers;
+  let vueForks = response.data.forks;
+  let vueStars = response.data.stargazers_count;
+  console.log("vue info:");
+  console.log(vueForks);
+  console.log(vueWatchers);
+  console.log(vueStars);
+});
+
+axios.get('https://api.github.com/repos/angular/angular.js').then(response => {
+  console.log(response.data);
+  let angularWatchers = response.data.watchers;
+  let angularForks = response.data.forks;
+  let angularStars = response.data.stargazers_count;
+  console.log("angular info:");
+  console.log(angularForks);
+  console.log(angularWatchers);
+  console.log(angularStars);
+});
+
+axios.get('https://api.github.com/repos/emberjs/ember.js').then(response => {
+  console.log(response.data);
+  let enberWatchers = response.data.watchers;
+  let enberForks = response.data.forks;
+  let enberStars = response.data.stargazers_count;
+  console.log("enber info:");
+  console.log(enberForks);
+  console.log(enberWatchers);
+  console.log(enberStars);
+});
+
+axios.get('https://api.github.com/repos/sveltejs/svelte').then(response => {
+  console.log(response.data);
+  let svelteWatchers = response.data.watchers;
+  let svelteForks = response.data.forks;
+  let svelteStars = response.data.stargazers_count;
+  console.log("svelte info:");
+  console.log(svelteForks);
+  console.log(svelteWatchers);
+  console.log(svelteStars);
+});
+
+axios.get('https://api.github.com/repos/facebook/react').then(response => {
+  console.log(response.data);
+  let reactWatchers = response.data.watchers;
+  let reactForks = response.data.forks;
+  let reactStars = response.data.stargazers_count;
+  console.log("react info:");
+  console.log(reactForks);
+  console.log(reactWatchers);
+  console.log(reactStars);
+});
+
+
 var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
   type: 'bar',
