dc05eabe29f20508b1bf0fc82291bae01a6a0b87
Yitzy Bratt <yitzybratt32@gmail.com>
2021-06-04 09:51:50 -0400

    gets stars forks and watches, displays in one chart

--- index.js
+++ index.js
@@ -50,33 +50,34 @@ console.log(allTheForks);
 console.log("Here are all the watchers:")
 console.log(allWatchers);
 
+const data = {
+  labels: names,
+  datasets: [
+    {
+      label: 'Stars',
+      data: allTheStars,
+      backgroundColor: 'rgba(54, 162, 235, 0.2)',
+      borderColor: 'rgba(54, 162, 235, 1)',
+    },
+    {
+      label: 'Forks',
+      data: allTheForks,
+      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      borderColor: 'rgba(153, 102, 255, 0.2)',
+    },
+    {
+      label: 'Watchers',
+      data: allWatchers,
+      backgroundColor: 'rgba(75, 192, 192, 0.2)',
+      borderColor: 'rgba(75, 192, 192, 1)',
+    }
+  ]
+};
+
 var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
   type: 'bar',
-  data: {
-    labels: names,
-    datasets: [{
-      label: 'Stars Count',
-      data: allTheStars,
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-      ],
-      borderWidth: 1
-    }]
-  },
+  data: data,
   options: {
     scales: {
       y: {
